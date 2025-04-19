8d2536b216a5fe918cdc5c949566099a6a4ce0d5
Kjeff24 <jeffreyarthur123@gmail.com>
2025-04-19 18:35:16 +0000

    Remove unused import

--- src/main/java/com/bexos/authorization_server/models/User.java
+++ src/main/java/com/bexos/authorization_server/models/User.java
@@ -16,7 +16,6 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.HashSet;
