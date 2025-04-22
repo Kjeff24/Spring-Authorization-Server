7e0820d72f35ac0381841d3dc646ac89ac45c5ad
Kjeff24 <jeffreyarthur123@gmail.com>
2025-04-22 09:55:00 +0000

    Added controller endpoint to save user

--- src/main/java/com/bexos/authorization_server/controller/AuthController.java
+++ src/main/java/com/bexos/authorization_server/controller/AuthController.java
@@ -1,14 +1,23 @@
 package com.bexos.authorization_server.controller;
 
+import com.bexos.authorization_server.dto.SignupRequest;
+import com.bexos.authorization_server.services.AuthService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error,
                         HttpSession session, Model model) {
@@ -31,6 +40,12 @@ public class AuthController {
 
     @GetMapping("/signup")
     public String signup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String formRegister(@ModelAttribute("user") SignupRequest signupRequest, RedirectAttributes redirectAttributes) {
+        authService.createUser(signupRequest, redirectAttributes);
         return "signup";
     }
 }
